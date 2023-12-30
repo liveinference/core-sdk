f8fe4aff84a6ac1c9fdf1b4ed9b4211e08cc7136
Sam Wen <samw@ritani.com>
2023-12-30 05:34:43 -0800

    add get_server_client

---
+++ src/get-server-client.ts
@@ -0,0 +1,27 @@
+
+import * as utils from "./utils";
+import { ServerClient } from "./server-client";
+import type * as types from './types';
+
+let serverClient: types.ServerClient | null = null;
+
+export default function getServerClient(
+    apiKey?: string, 
+    apiBaseUrl?: string
+) : types.ServerClient 
+{
+    if (!serverClient) {
+        if (!apiKey) {
+            if (process.env.LIVE_INFERENCE_API_KEY) {
+                apiKey = process.env.LIVE_INFERENCE_API_KEY;
+            } else {
+                throw new Error("LIVE_INFERENCE_API_KEY is not set");
+            }
+        }
+        if (apiBaseUrl) {
+            utils.setApiBaseUrl(apiBaseUrl);
+        }
+        serverClient = new ServerClient({ apiKey });
+    }
+    return serverClient;
+}
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,17 @@ import createClient from "./create-client";
 import { LiveInference } from "./live-inference";
 import { SrcsStream } from "./srcs-stream";
 import { MediaStream } from "./media-stream";
+import getServerClient from "./get-server-client";
 
 export type * from "./types";
-export { utils, Base, Client, ServerClient, createClient, LiveInference, SrcsStream, MediaStream };
+export { 
+    utils, 
+    Base, 
+    Client, 
+    ServerClient, 
+    createClient, 
+    LiveInference, 
+    SrcsStream, 
+    MediaStream, 
+    getServerClient 
+};
